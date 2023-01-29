10cf379bcb27fd2fbe012fe785e7088025da953d
pawel <pawel.drozdz.94@gmail.com>
2023-01-29 20:43:49 +0100

    changed property list, property service

--- app/services/property-service.js
+++ app/services/property-service.js
@@ -13,7 +13,7 @@ let prettifyProperty = (property) => {
         city: property.city__c,
         state: property.state__c,
         price: property.price__c,
-        priceFormatted: "$" + property.price__c.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+        priceFormatted: property.price__c.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "zł",
         beds: property.beds__c,
         baths: property.baths__c,
         description: property.description__c,
